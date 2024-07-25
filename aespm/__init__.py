2c84785ce98a7240811716e98817217fee6407af
Slautin <bslautin@gmail.com>
2024-07-25 07:52:25 +0200

    remove if not os.path.exists

--- aespm/__init__.py
+++ aespm/__init__.py
@@ -30,24 +30,24 @@ if platform.system() == 'Windows':
 			with open(os.path.join(buffer_path, 'ToIgor.arcmd'), 'w') as fopen:
 				fopen.write(' ')
 		# Create bash file to execute commands in hte buffer file
-		if not os.path.exists(os.path.join(buffer_path, 'SendToIgor.bat')):
-			with open(os.path.join(buffer_path, 'SendToIgor.bat'), 'w') as fopen:
-				if os.path.exists("C:\\Asylum Research\\v19"):
-					fopen.write('"C:\\Asylum Research\\v19\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
-				elif os.path.exists("C:\\Asylum Research\\v18"):
-					fopen.write('"C:\\Asylum Research\\v18\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
-				elif os.path.exists("C:\\Asylum Research\\v17"):
-					fopen.write('"C:\\Asylum Research\\v17\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
-				elif os.path.exists("C:\\Asylum Research\\v16"):
-					fopen.write('"C:\\Asylum Research\\v16\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
-				elif os.path.exists("C:\\Asylum\\Igor Pro Folder AR15"):
-					fopen.write('"C:\\Asylum\\Igor Pro Folder AR15\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
-				elif os.path.exists("C:\\Asylum Research\\v14"):
-					fopen.write('"C:\\Asylum Research\\v14\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
-				elif os.path.exists("C:\\Asylum Research\\v13"):
-					fopen.write('"C:\\Asylum Research\\v13\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
-				else:
-					print("No supported AR versions!")
+		#if not os.path.exists(os.path.join(buffer_path, 'SendToIgor.bat')):
+		with open(os.path.join(buffer_path, 'SendToIgor.bat'), 'w') as fopen:
+			if os.path.exists("C:\\Asylum Research\\v19"):
+				fopen.write('"C:\\Asylum Research\\v19\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
+			elif os.path.exists("C:\\Asylum Research\\v18"):
+				fopen.write('"C:\\Asylum Research\\v18\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
+			elif os.path.exists("C:\\Asylum Research\\v17"):
+				fopen.write('"C:\\Asylum Research\\v17\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
+			elif os.path.exists("C:\\Asylum Research\\v16"):
+				fopen.write('"C:\\Asylum Research\\v16\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
+			elif os.path.exists("C:\\Asylum\\Igor Pro Folder AR15"):
+				fopen.write('"C:\\Asylum\\Igor Pro Folder AR15\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
+			elif os.path.exists("C:\\Asylum Research\\v14"):
+				fopen.write('"C:\\Asylum Research\\v14\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
+			elif os.path.exists("C:\\Asylum Research\\v13"):
+				fopen.write('"C:\\Asylum Research\\v13\\RealTime\\Igor Pro Folder\\Igor.exe" "{}"'.format(os.path.join(buffer_path, 'ToIgor.arcmd')))
+			else:
+				print("No supported AR versions!")
 
 		# Copy the user functions into the default include folder of AR
 		# shutil.copy(os.path.join(aespm.__path__, 'user functions', 'UserFunctions.ipf'), os.path.join(doc_path, 'AsylumResearch', 'UserIncludes'))
